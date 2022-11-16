067ae49cb7160307ea540a5f2253ded35b471837
Masumraihan <mdmasumraihan1@gamil.com>
2022-11-17 02:05:46 +0600

    some idea in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
+const person ={
+  name: "salman khan",
+  style: "taklu"
+}
+const person2 = {
+  name : "eve rahman",
+  style: "nothing"
+}
+const style = {
+  backgroundColor: 'pink',
+  color:'black',
+  padding:'10px'
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1 style={style}>first person: {person.name + " " + person.style}</h1>
+        <h2 style={{backgroundColor:'cyan',padding:'10px',color:'black'}}>second person: {person2.name + " " + person2.style}</h2>
+        <Details></Details>
+        <Details></Details>
+        <Details></Details>
+        <Details></Details>
+        
       </header>
     </div>
   );
 }
+function Details(){
+  const textStyle = {
+    backgroundColor:'cyan',
+    margin:'20px',
+    padding:'10px 20px',
+    borderRadius:'10px'
+  }
+  return (
+    <div style={textStyle} >
+      <h1 style={{color:'black'}}>Name: Masum Raihan</h1>
+      <h3>job: Web developer</h3>
+    </div>
+  )
+}
+
 
 export default App;
